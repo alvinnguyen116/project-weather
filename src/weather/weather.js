79c37c3d72b5b5de2472b7e5844153491248981d
alvin <@>
2020-04-23 18:17:24 -0700

    milestone 2 time limit

--- src/weather/weather.js
+++ src/weather/weather.js
@@ -7,10 +7,17 @@ import './weather.scss';
  */
 function Weather({isGoogleReady, isD3Ready, isSearchMode, address, setInputVal, inputVal}) {
 
+    const BUTTON = Object.freeze({
+       TEMPERATURE: "0",
+       PRECIPITATION: "1",
+       WIND: "2"
+    });
+
     const [currentLocation, setCurrentLocation] = useState();
     const [currentUnit, setCurrentUnit] = useState(UNIT.FAHRENHEIT);
     const [currentWeather, setCurrentWeather] = useState(null);
     const [currentDay, setCurrentDay] = useState(0);
+    const [currentButton, setCurrentButton] = useState(BUTTON.TEMPERATURE);
     const svgRef = useRef(null);
 
     // SIDE EFFECTS ----------------------------------------------------------------------------------------------------
@@ -18,9 +25,54 @@ function Weather({isGoogleReady, isD3Ready, isSearchMode, address, setInputVal, 
     useEffect(() => {
         /*global d3*/
         if(isD3Ready && currentWeather && currentWeather.hourly && currentWeather.hourly.length) {
-            d3.select(svgRef.current).attr("width", 400).attr("height", 200);
+            const width = 618;
+            const height = 56;
+            const data = [];
+            currentWeather.hourly.slice(0,24).forEach(({dt, temp, wind_speed, rain}, i) => {
+               let value;
+               switch (currentButton) {
+                   case BUTTON.TEMPERATURE:
+                       value = temp;
+                       break;
+                   case BUTTON.PRECIPITATION:
+                       value = rain ? rain['1h'] : 0;
+                       break;
+                   case BUTTON.WIND:
+                       value = wind_speed;
+                       break;
+                   default:
+                       break;
+               }
+               data.push({i,value});
+            });
+
+            const svg = d3.select(svgRef.current)
+                .attr("width", width)
+                .attr("height", height);
+            svg.selectAll("g").remove();
+            const g = svg.append('g');
+
+            const x = d3.scaleTime()
+                .rangeRound([0, width])
+                .domain([0, 23]);
+            let ydomain = d3.extent(data.map(d => d.value));
+            if (currentButton === BUTTON.PRECIPITATION) {
+                ydomain = [0,1];
+            }
+            const y = d3.scaleLinear()
+                .rangeRound([height, 0])
+                .domain(ydomain);
+            const line = d3.line()
+                .x(d => x(d.i))
+                .y(d => Math.round(y(d.value)));
+
+            g.append("path").datum(data)
+                .attr("stroke", "#ec6e4c")
+                .attr("stroke-width", 2)
+                .attr("fill", "none")
+                .attr("d", line);
         }
-    }, [currentWeather]);
+    }, [currentWeather, isD3Ready, currentButton]);
 
     useEffect(() => {
         if (currentLocation) {
@@ -122,13 +174,13 @@ function Weather({isGoogleReady, isD3Ready, isSearchMode, address, setInputVal, 
                             </div>
                         </div>
                         <div className={"right"}>
-                            <div className={"precipitation"}>Precipitation: {`${rain ? rain : 0}%`}</div>
+                            <div className={"precipitation"}>Precipitation: {`${rain ? rain['1h'] : 0}%`}</div>
                             <div className={"humidity"}>Humidity: {`${humidity}%`}</div>
                             <div className={"wind"}>Wind: {`${wind_speed} mph`}</div>
                             <div className={"buttons"}>
-                                <button>Temperature</button>
-                                <button>Precipitation</button>
-                                <button>Wind</button>
+                                <button onClick={() => setCurrentButton(BUTTON.TEMPERATURE)}>Temperature</button>
+                                <button onClick={() => setCurrentButton(BUTTON.PRECIPITATION)}>Precipitation</button>
+                                <button onClick={() => setCurrentButton(BUTTON.WIND)}>Wind</button>
                             </div>
                         </div>
                     </div>
